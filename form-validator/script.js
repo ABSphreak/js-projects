a86dafa0079c1d33f72f6c801a6b0ebbc7919c5d
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-08-24 00:31:02 +0530

    📑 Form Validator - Simple Validation

---
+++ form-validator/script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+};
+
+// Show success outline
+const showSuccess = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+};
+
+// Event Listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, "Username is required!");
+  } else {
+    showSuccess(username);
+  }
+});
