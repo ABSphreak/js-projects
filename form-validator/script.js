2c3274af91c3da238371d489f2976a4ab7e3e89a
ABSphreak <absphreak@outlook.com>
2020-08-26 00:03:17 +0530

    💦 Simple Validation

--- form-validator/script.js
+++ form-validator/script.js
@@ -18,6 +18,12 @@ const showSuccess = (input, message) => {
   formControl.className = "form-control success";
 };
 
+// Check email is valid
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
 // Event Listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -30,6 +36,8 @@ form.addEventListener("submit", (e) => {
 
   if (email.value === "") {
     showError(email, "Email is required!");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid!");
   } else {
     showSuccess(email);
   }
